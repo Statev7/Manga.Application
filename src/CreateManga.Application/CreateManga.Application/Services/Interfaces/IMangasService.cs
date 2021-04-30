6cead348722d3568beb7e161822b999c4aa78e43
Statev07 <42496790+Statev07@users.noreply.github.com>
2021-05-01 01:56:01 +0300

    Create IMangasService.cs

---
+++ src/CreateManga.Application/CreateManga.Application/Services/Interfaces/IMangasService.cs
@@ -0,0 +1,27 @@
+﻿namespace CreateManga.Application.Services.Interfaces
+{
+    using CreateManga.Application.Data.Models;
+    using CreateManga.Application.Models.Mangas.BindingModels;
+    using CreateManga.Application.Models.Mangas.ViewModels;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IMangasService
+    {
+        IEnumerable<MangaViewModel> GetAll();
+
+        MangaViewModel GetDetailsById(int id);
+
+        Manga GetByModelName(string modelName);
+
+        Task CreateAsync(CreateMangaBindingModel model);
+
+        UpdateMangaBiningModel UpdateById(int id);
+
+        Task UpdateAsync(UpdateMangaBiningModel model);
+
+        Task DeleteAsync(int id);
+    }
+}
