0b188396a211b5b66200a2578fd5d92387fdde6e
Statev07 <42496790+Statev07@users.noreply.github.com>
2021-05-15 16:39:01 +0300

    Update CreateChaptersBindingModel.cs

--- src/CreateManga.Application/CreateManga.Application/Areas/Designing/Models/Chapters/BindingModels/CreateChaptersBindingModel.cs
+++ src/CreateManga.Application/CreateManga.Application/Areas/Designing/Models/Chapters/BindingModels/CreateChaptersBindingModel.cs
@@ -1,11 +1,7 @@
 ﻿namespace CreateManga.Application.Areas.Designing.Chapters.BindingModels
 {
-    using System;
-    using System.Collections.Generic;
     using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     public class CreateChaptersBindingModel
     {
