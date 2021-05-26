268d00b5da2185df6befbacfbb5b26e7090f0958
Statev07 <42496790+Statev07@users.noreply.github.com>
2021-05-26 13:47:29 +0300

    Add Notification.

---
+++ src/CreateManga.Application/CreateManga.Application/Constants/NotificationsConstants/NotificationsConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace CreateManga.Application.Constants.NotificationsConstants
+{
+    public static class NotificationsConstants
+    {
+        public const string SUCCESS_NOTIFICATION = "Success";
+        public const string ERROR_NOTIFICATION = "Error";
+        public const string WARNING_NOTIFICATION = "Warning";
+        public const string INFO_NNOTIFICATION = "Info";
+
+        public const string FAILED_TO_SET_DATE = "The start date cannot be later than the end date";
+    }
+}
---
+++ src/CreateManga.Application/CreateManga.Application/Data/Seed/ApplicationDbContextSeeder.cs
@@ -0,0 +1,37 @@
+﻿namespace CreateManga.Application.Data.Seeders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using CreateManga.Application.Data.Seed.Seeders;
+    using CreateManga.Application.Data.Seed.Seeders.Interfaces;
+
+    public class ApplicationDbContextSeeder
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ApplicationDbContextSeeder(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task SeedDatabaseAsync()
+        {
+            var dbContext = this.serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+            List<ISeeder> seeders = new List<ISeeder>()
+            {
+                new RolesSeeder(),
+                new UsersSeeder(),
+            };
+
+            foreach (var seeder in seeders)
+            {
+                await seeder.SeedAsync(dbContext, serviceProvider);
+            }
+        }
+    }
+}
