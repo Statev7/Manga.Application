5db8a1a7ea807042d61d275abaad28827b2b6d34
Statev07 <42496790+Statev07@users.noreply.github.com>
2021-06-01 21:20:22 +0300

    Add top button.

---
+++ src/CreateManga.Application/CreateManga.Application/wwwroot/js/GoTopButton.js
@@ -0,0 +1,13 @@
+﻿window.onscroll = function () { scroll() };
+
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("GoTopBtn").style.display = "block";
+    } else {
+        document.getElementById("GoTopBtn").style.display = "none";
+    }
+}
+
+function GoTopFunction() {
+    document.documentElement.scrollTop = 0;
+}
--- src/CreateManga.Application/CreateManga.Application/wwwroot/js/site.js
+++ src/CreateManga.Application/CreateManga.Application/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+}
